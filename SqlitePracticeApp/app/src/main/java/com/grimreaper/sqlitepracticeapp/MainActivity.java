cf92a2da438145a13ebc67854bb4138901216ca6
Rizwan Atta <rizwanatta890@gmail.com>
2017-08-10 04:00:20 -0700

    Get or set data into sqlite db methods

--- SqlitePracticeApp/app/src/main/java/com/grimreaper/sqlitepracticeapp/MainActivity.java
+++ SqlitePracticeApp/app/src/main/java/com/grimreaper/sqlitepracticeapp/MainActivity.java
@@ -1,35 +1,39 @@
 package com.grimreaper.sqlitepracticeapp;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.grimreaper.sqlitepracticeapp.dbmsPkg.DBMS;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText edtTxtName,edtTxtFName,edtTxtPass,edtTxtAddress;
-    String  name,fname,pass,address;
-    Button btnRegister;
+    EditText edtTxtName, edtTxtFName, edtTxtPass, edtTxtAddress;
+    String name, fname, pass, address;
+    Button btnRegister,btnGetAllUsers;
 
-//    our custom Database Manager instance
+    //    our custom Database Manager instance
     DBMS dbms;
-//    Helper to close or open the DB
+    //    Helper to close or open the DB
     SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dbms = new DBMS(MainActivity.this);
         edtTxtName = (EditText) findViewById(R.id.edt_txt_name);
         edtTxtFName = (EditText) findViewById(R.id.edt_txt_fname);
         edtTxtPass = (EditText) findViewById(R.id.edt_txt_pass);
         edtTxtAddress = (EditText) findViewById(R.id.edt_txt_address);
 
         btnRegister = (Button) findViewById(R.id.btn_register);
+        btnGetAllUsers = (Button) findViewById(R.id.btn_get_all_users);
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,17 +44,46 @@ public class MainActivity extends AppCompatActivity {
                 pass = edtTxtPass.getText().toString();
                 address = edtTxtAddress.getText().toString();
 
-                if(name.isEmpty()||fname.isEmpty()||pass.isEmpty()||address.isEmpty()){
+                if (name.isEmpty() || fname.isEmpty() || pass.isEmpty() || address.isEmpty()) {
                     edtTxtName.setError("Form Not Filled");
                     edtTxtFName.setError("Form Not Filled");
                     edtTxtPass.setError("Form Not Filled");
                     edtTxtAddress.setError("Form Not Filled");
-                }else{
+                } else {
 //                    here we will use the DBMS features
+                    db = dbms.getWritableDatabase();
+                    dbms.insertNewUserIntoDb(db,name,fname,pass,address);
+                    edtTxtName.setText("");
+                    edtTxtFName.setText("");
+                    edtTxtPass.setText("");
+                    edtTxtAddress.setText("");
+
+
                 }
 
 
             }
         });
+
+        btnGetAllUsers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db = dbms.getReadableDatabase();
+                Cursor incomingCursor = dbms.getAllRegisteredUsers(db);
+                if(incomingCursor==null){
+                    Toast.makeText(MainActivity.this, "NO data in db", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "data is in db", Toast.LENGTH_SHORT).show();
+                    if(incomingCursor.moveToFirst()){
+                     do{
+                            String User = incomingCursor.getString(0)
+                                    +"\t"+incomingCursor.getString(1)+"\t"+incomingCursor.getString(2)+"\t"+incomingCursor.getString(3);
+                         Toast.makeText(MainActivity.this, User, Toast.LENGTH_SHORT).show();
+                     }   while(incomingCursor.moveToNext());
+                    }
+                }
+            }
+        });
+
     }
 }
--- SqlitePracticeApp/app/src/main/java/com/grimreaper/sqlitepracticeapp/dbmsPkg/DBMS.java
+++ SqlitePracticeApp/app/src/main/java/com/grimreaper/sqlitepracticeapp/dbmsPkg/DBMS.java
@@ -1,6 +1,8 @@
 package com.grimreaper.sqlitepracticeapp.dbmsPkg;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -10,24 +12,48 @@ import android.util.Log;
  */
 
 public class DBMS extends SQLiteOpenHelper {
-    public static String DBName  ="UserDatabase.db";
-    public static int DBVersion  =1;
-    public String UserDetailTableQuery  = "CREATE TABLE user_details(p_name TEXT,p_Fname TEXT,p_password TEXT,P_address TEXT)";
+    public static String DBName = "UserDatabase.db";
+    public static int DBVersion = 1;
+    public String UserDetailTableQuery = "CREATE TABLE user_details(p_name TEXT,p_Fname TEXT,p_password TEXT,P_address TEXT)";
 
     public DBMS(Context context) {
         super(context, DBName, null, DBVersion);
-        Log.e("DBMS=>","yes Db bna d ha");
+        Log.e("DBMS=>", "yes Db bna d ha");
     }
 
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(UserDetailTableQuery);
-        Log.e("ONCREATE=>","yes db ma table bn geya ha");
+        Log.e("ONCREATE=>", "yes db ma table bn geya ha");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    public void insertNewUserIntoDb(SQLiteDatabase db, String Name, String Fname, String Pass, String Address) {
+        ContentValues cv = new ContentValues();
+        cv.put("p_name", Name);
+        cv.put("p_Fname", Fname);
+        cv.put("p_password", Pass);
+        cv.put("P_address", Address);
+        db.insert("user_details", null, cv);
+        Log.e("insertNewUserIntoDb: ", "data is inserted");
+    }
+
+
+    public Cursor getAllRegisteredUsers(SQLiteDatabase db) {
+
+        String[] projections = {
+                "p_name",
+                "p_Fname",
+                "p_password",
+                "P_address"
+        };
+
+        Cursor mCursor =  db.query("user_details",projections,null,null,null,null,null);
+        return mCursor;
     }
 }
