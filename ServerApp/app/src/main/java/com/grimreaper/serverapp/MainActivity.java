aa6ea407d16bcdb5b20ee89dfd100fce4903bb7f
Rizwan Atta <rizwanatta890@gmail.com>
2017-08-16 04:46:20 -0700

    android with PREFAB URLS FOR SERVER BASED APPS

--- ServerApp/app/src/main/java/com/grimreaper/serverapp/MainActivity.java
+++ ServerApp/app/src/main/java/com/grimreaper/serverapp/MainActivity.java
@@ -1,10 +1,14 @@
 package com.grimreaper.serverapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -12,18 +16,89 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtTxtName, edtTxtFName, edtTxtPass, edtTxtAddress;
+    String name, fname, pass, address;
+    Button btnRegister,btnGetAllUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtTxtName = (EditText) findViewById(R.id.edt_txt_name);
+        edtTxtFName = (EditText) findViewById(R.id.edt_txt_fname);
+        edtTxtPass = (EditText) findViewById(R.id.edt_txt_pass);
+        edtTxtAddress = (EditText) findViewById(R.id.edt_txt_address);
+
+        btnRegister = (Button) findViewById(R.id.btn_register);
+        btnGetAllUsers = (Button) findViewById(R.id.btn_get_all_users);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                /taking all the entered values from the user
+                name = edtTxtName.getText().toString();
+                fname = edtTxtFName.getText().toString();
+                pass = edtTxtPass.getText().toString();
+                address = edtTxtAddress.getText().toString();
+
+                if (name.isEmpty() || fname.isEmpty() || pass.isEmpty() || address.isEmpty()) {
+                    edtTxtName.setError("Form Not Filled");
+                    edtTxtFName.setError("Form Not Filled");
+                    edtTxtPass.setError("Form Not Filled");
+                    edtTxtAddress.setError("Form Not Filled");
+                } else {
+//                    here we will use the DBMS features
+
+                }
+
+
+            }
+        });
+
+        btnGetAllUsers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
-    public void BringMeTheServerResponse(View v){
+
+//    Insert New User Method
+public void InsertNewUser(String Name,String Email,String Pass,String Address){
+
+    //        StringRequest mStringRequest = new StringRequest(Request.Method.POST,);
+    StringRequest mStringRequest = new StringRequest(1,getApplicationContext().getResources().getString(R.string.InsertNewUserUrl) , new Response.Listener<String>() {
+        @Override
+        public void onResponse(String response) {
+            Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+        }
+    }, new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Toast.makeText(MainActivity.this, "Server side error", Toast.LENGTH_SHORT).show();
+        }
+    }){
+        @Override
+        protected Map<String, String> getParams() throws AuthFailureError {
+            return super.getParams();
+        }
+    };
+
+    RequestQueue mRequestQueue = Volley.newRequestQueue(MainActivity.this);
+    mRequestQueue.add(mStringRequest);
+}
+
+
+
+    public void BringMeTheServerResponse(){
 
 //        StringRequest mStringRequest = new StringRequest(Request.Method.POST,);
-        StringRequest mStringRequest = new StringRequest(1, "http://192.168.2.106:8080/serverApp/testScript.php", new Response.Listener<String>() {
+        StringRequest mStringRequest = new StringRequest(1,getApplicationContext().getResources().getString(R.string.basic_connection_url) , new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
